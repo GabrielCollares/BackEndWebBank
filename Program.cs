1dec75f45d5032e70d0f05fe0766fa6d4555e3e8
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-06-07 03:47:50 +0000

    .

--- Program.cs
+++ Program.cs
@@ -3,15 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<AppDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
