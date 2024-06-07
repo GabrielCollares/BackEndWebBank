f375d81de5987c6d6aec43400a90d106abf985d0
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-06-07 03:06:12 +0000

    cfg

--- BackendSharp/obj/Debug/net8.0/BackendSharp.AssemblyInfo.cs
+++ BackendSharp/obj/Debug/net8.0/BackendSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c902a6363db51dca74e598f2501010f580231e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
